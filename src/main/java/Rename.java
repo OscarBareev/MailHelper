beb0a3fe52bb94502e0b43f9e7bd69e1e41c042a
Oscar Bareev <oscar.bareev@gmail.com>
2021-09-22 10:36:23 +0300

    done

--- src/main/java/Rename.java
+++ src/main/java/Rename.java
@@ -12,7 +12,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.stream.Stream;
 
